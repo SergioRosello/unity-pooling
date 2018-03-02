e0738efcb1ecc88e474b7040985e4dc1e176ec1b
SergioRosello <sergio-rosello@hotmail.com>
2018-03-02 16:45:21 +0100

    Finished Pooling

---
+++ Unity-pooling/Pooling/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour {
+	public void ClickedButton() {
+		SceneManager.LoadScene(0);
+	}
+}
---
+++ Unity-pooling/Pooling/Assets/Scripts/PoolInfoPrinter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoolInfoPrinter : MonoBehaviour {
+
+	private Text text;
+	public string prefabName;
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(PoolManager.Instance.getNumberOfInstancesOfPrefab(prefabName) != -1)
+			text.text = prefabName + " Has: " + PoolManager.Instance.getNumberOfInstancesOfPrefab(prefabName).ToString() + " Instances.";
+		else{
+			text.text = "none";
+		}
+	}
+}
